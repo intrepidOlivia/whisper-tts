39c8884a9976e85f964419e3fe37b727c3e33e57
intrepidOlivia <aethereal@gmail.com>
2024-01-12 13:59:19 -0800

    Add auth key setup flow

--- src/extension/popup.js
+++ src/extension/popup.js
@@ -1,25 +1,36 @@
-function initializePopup() {
-    const storedKey = chrome.storage.local.get('openai_key');
-    if (storedKey) {
-        showLoggedInUI();
-    } else {
+window.onload = initializePopup;
+
+async function initializePopup() {
+    // Event Handlers
+    const form = document.querySelector('form');
+    form.addEventListener('submit', saveOpenAIKey);
+    const unsaveButton = document.querySelector('#unsave_key_btn');
+    unsaveButton.addEventListener('click', unsaveOpenAIKey);
+
+    const bytesStored = await chrome.storage.local.getBytesInUse('openai_key');
+    if (bytesStored === 0) {
         showLoggedOutUI();
+    } else {
+        showLoggedInUI();
     }
 }
 
-async function saveOpenAIKey(value) {
-    console.log('What is value?', value);
-    const keyValue = "";
-    // const keyValue = document.querySelector('')
+async function saveOpenAIKey(submitEvent) {
+    submitEvent.preventDefault();
+    let keyValue;
+    const  input = submitEvent.target.querySelector('#openai-key-input');
+    if (input) {
+        keyValue = input.value;
+    }
     // Make an auth check to make sure the key works
-    response = await fetch('https://api.openai.com/v1/models', { headers: { Authorization: keyValue }});
+    response = await fetch('https://api.openai.com/v1/models', { headers: { Authorization: `Bearer ${keyValue}` }});
     if (response.status !== 200) {
         window.alert('Provided API key was not recognized as valid.');
         return;
     }
     
     // Store OpenAI key in localStorage
-    chrome.storage.local.set({ openai_key: keyValue});
+    await chrome.storage.local.set({ openai_key: keyValue});
 
     // Switch UI
     showLoggedInUI();
@@ -27,14 +38,15 @@ async function saveOpenAIKey(value) {
     return false;
 }
 
-function unsaveOpenAIKey() {
-    chrome.storage.local.clear();
+async function unsaveOpenAIKey() {
+    await chrome.storage.local.clear();
+    showLoggedOutUI();
 }
 
-function showLoggedInUI() {
+async function showLoggedInUI() {
     document.querySelector('#not-logged-in').classList.add('hidden');
     document.querySelector('#logged-in').classList.remove('hidden');
-    const storedKey = chrome.storage.local.get('openai_key');
+    const storedKey = await chrome.storage.local.get('openai_key');
     document.querySelector('#saved-openai-key').value = storedKey;
 }
 
